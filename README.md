e809d66bc3afd01b0a950e7e7725ee3d74ddb8a6
vchandwani <varunchandwani@gmail.com>
2018-02-20 16:59:29 +1100

    New changes
    New changes

---
+++ README.md
@@ -0,0 +1,4 @@
+# Angular2Mean
+Requires Angular, NodeJs, MongoDB and ExpressJS
+Node JS  : v6.7.0
+Dont change connections file unless required
---
+++ assets/app/auth/user.model.ts
@@ -0,0 +1,4 @@
+export class User {
+    
+    constructor(public email:string,public pasword:string, public firstName?: string, public lastName?: string){}
+}

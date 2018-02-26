5b7a53073bd59df0491ea7342c10baa203547178
vchandwani <varunchandwani@gmail.com>
2018-02-26 13:57:03 +1100

    Prod build
    Prod build

---
+++ assets/app/app.component.js
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+    }
+    AppComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'my-app',
+                    templateUrl: './app.component.html'
+                },] },
+    ];
+    /** @nocollapse */
+    AppComponent.ctorParameters = function () { return []; };
+    return AppComponent;
+}());
+export { AppComponent };
